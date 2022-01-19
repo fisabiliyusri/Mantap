1c468d2c1a02a695cf06aba3c929844d71a977f3
Seno Wahyu Hidayat <70935444+senowahyu62@users.noreply.github.com>
2022-01-19 20:39:31 +0700

    Update ins-xray.sh

--- xray/ins-xray.sh
+++ xray/ins-xray.sh
@@ -14,7 +14,7 @@ LIGHT='\033[0;37m'
 
 MYIP=$(wget -qO- ipinfo.io/ip);
 clear
-domain=$(cat /root/domain)
+domain=$(cat /etc/xray/domain)
 apt install iptables iptables-persistent -y
 apt install curl socat xz-utils wget apt-transport-https gnupg gnupg2 gnupg1 dnsutils lsb-release -y 
 apt install socat cron bash-completion ntpdate -y
