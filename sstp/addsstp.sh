064dfee732dbd5552a06f1ae49257bdcf39080d6
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-02-06 05:34:25 +0700

    Update addsstp.sh

--- sstp/addsstp.sh
+++ sstp/addsstp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# My Telegram : https://t.me/Akbar218
+# SL
 # ==========================================
 # Color
 RED='\033[0;31m'
@@ -14,19 +14,16 @@ LIGHT='\033[0;37m'
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/senowahyu62/perizinan/main/ipvps.txt | grep $MYIP )
+IZIN=$( curl ipinfo.io/ip | grep $MYIP )
 if [ $MYIP = $MYIP ]; then
 echo -e "${NC}${GREEN}Permission Accepted...${NC}"
 else
 echo -e "${NC}${RED}Permission Denied!${NC}";
-echo -e "${NC}${LIGHT}Please Contact Admin!!"
-echo -e "${NC}${LIGHT}Facebook : https://m.facebook.com/lis.tio.718"
-echo -e "${NC}${LIGHT}WhatsApp : 081545854516"
-echo -e "${NC}${LIGHT}Telegram : https://t.me/Akbar218"
+echo -e "${NC}${LIGHT}Fuck You!!"
 exit 0
 fi
 clear
-source /var/lib/akbarstorevpn/ipvps.conf
+source /var/lib/crot/ipvps.conf
 if [[ "$IP2" = "" ]]; then
 domain=$(cat /etc/xray/domain)
 else
@@ -36,7 +33,7 @@ IP=$(wget -qO- ipinfo.io/ip);
 sstp="$(cat ~/log-install.txt | grep -i SSTP | cut -d: -f2|sed 's/ //g')"
 until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
 		read -rp "Usernew : " -e user
-		CLIENT_EXISTS=$(grep -w $user /var/lib/akbarstorevpn/data-user-sstp | wc -l)
+		CLIENT_EXISTS=$(grep -w $user /var/lib/crot/data-user-sstp | wc -l)
 
 		if [[ ${CLIENT_EXISTS} == '1' ]]; then
 			echo ""
@@ -51,7 +48,7 @@ exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
 cat >> /home/sstp/sstp_account <<EOF
 $user * $pass *
 EOF
-echo -e "### $user $exp">>"/var/lib/akbarstorevpn/data-user-sstp"
+echo -e "### $user $exp">>"/var/lib/crot/data-user-sstp"
 clear
 cat <<EOF
 
@@ -67,5 +64,5 @@ Cert      : http://$IP:89/server.crt
 Created   : $hariini
 Expired   : $exp
 ============================
-Script By Akbar Maulana
+Script Mod By SL
 EOF
