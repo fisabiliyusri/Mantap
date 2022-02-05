b15f087a7ce42383fef0a992b3139d0799713422
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-02-06 06:52:11 +0700

    Update sodosok.sh

--- shadowsocks/sodosok.sh
+++ shadowsocks/sodosok.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-#shadowsocks-libev obfs install by Akbar Maulana
-# My Telegram : https://t.me/Akbar218
+#shadowsocks-libev obfs install by SL
 # ==========================================
 # Color
 RED='\033[0;31m'
@@ -15,19 +14,16 @@ LIGHT='\033[0;37m'
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/senowahyu62/perizinan/main/ipvps.txt | grep $MYIP )
+IZIN=$( curl ipinfo.io/ip | grep $MYIP )
 if [ $MYIP = $MYIP ]; then
 echo -e "${NC}${GREEN}Permission Accepted...${NC}"
 else
 echo -e "${NC}${RED}Permission Denied!${NC}";
-echo -e "${NC}${LIGHT}Please Contact Admin!!"
-echo -e "${NC}${LIGHT}Facebook : https://m.facebook.com/lis.tio.718"
-echo -e "${NC}${LIGHT}WhatsApp : 081545854516"
-echo -e "${NC}${LIGHT}Telegram : https://t.me/Akbar218"
+echo -e "${NC}${LIGHT}Fuck You!!"
 exit 0
 fi
 # Link Hosting Kalian
-akbarvpn="raw.githubusercontent.com/senowahyu62/scriptvps/main/shadowsocks"
+akbarvpn="raw.githubusercontent.com/fisabiliyusri/Mantap/main/shadowsocks"
 
 source /etc/os-release
 OS=$ID
