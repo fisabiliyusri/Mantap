2ae9ec3ca9596fe93f7552b11ba3a26b082178e4
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-02-07 08:34:57 +0700

    Create vpsroot.sh

---
+++ vpsroot.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Mod By SL
+wget -qO- -O /etc/ssh/sshd_config https://raw.githubusercontent.com/fisabiliyusri/Mantap/vpsroot/main/sshd_config;
+systemctl restart sshd;
+clear;
+echo -e "Masukkan Password:";
+read -e pwe;
+usermod -p `perl -e "print crypt("$pwe","Q4")"` root;
+clear;
+printf "Mohon Simpan Informasi Akun VPS Ini
+============================================
+Ip address = $(curl -Ls http://ipinfo.io/ip)
+Username   = root
+Password   = $pwe
+============================================";
+echo "";
+exit;
