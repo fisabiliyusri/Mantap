bf11f505e5e02556766cc3c182aec5c1bced2a3f
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-02-16 12:39:19 +0700

    Create addxvgrpc.sh

---
+++ xray/addxvgrpc.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+# SL
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+IZIN=$( curl ipinfo.io/ip | grep $MYIP )
+if [ $MYIP = $MYIP ]; then
+echo -e "${NC}${GREEN}Permission Accepted...${NC}"
+else
+echo -e "${NC}${RED}Permission Denied!${NC}";
+echo -e "${NC}${LIGHT}Fuck You!!"
+exit 0
+fi
+clear
+source /var/lib/crot/ipvps.conf
+if [[ "$IP" = "" ]]; then
+domain=$(cat /etc/xray/domain)
+else
+domain=$IP
+fi
+tls=$(cat /etc/xray/vmessgrpc.json | grep port | awk '{print $2}' | sed 's/,//g')
+vl=$(cat /etc/xray/vlessgrpc.json | grep port | awk '{print $2}' | sed 's/,//g')
+until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
+		read -rp "User: " -e user
+		CLIENT_EXISTS=$(grep -w $user /etc/xray/vmessgrpc.json | wc -l)
+
+		if [[ ${CLIENT_EXISTS} == '1' ]]; then
+			echo ""
+			echo "A client with the specified name was already created, please choose another name."
+			exit 1
+		fi
+	done
+uuid=$(cat /proc/sys/kernel/random/uuid)
+exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
+sed -i '/#vmessgrpc$/a\### '"$user $exp"'\
+},{"id": "'""$uuid""'","alterId": '"0"',"email": "'""$user""'"' /etc/xray/vmessgrpc.json
+sed -i '/#vlessgrpc$/a\### '"$user $exp"'\
+},{"id": "'""$uuid""'","email": "'""$user""'"' /etc/xray/vlessgrpc.json
+cat > /etc/xray/$user-tls.json << EOF
+      {
+      "v": "0",
+      "ps": "${user}",
+      "add": "${domain}",
+      "port": "${tls}",
+      "id": "${uuid}",
+      "aid": "0",
+      "net": "grpc",
+      "path": "GunService",
+      "type": "none",
+      "host": "${sni}",
+      "tls": "tls"
+}
+EOF
+vmess_base641=$( base64 -w 0 <<< $vmess_json1)
+vmesslink1="vmess://$(base64 -w 0 /etc/xray/$user-tls.json)"
+vlesslink1="vless://${uuid}@${domain}:${vl}?mode=gun&security=tls&encryption=none&type=grpc&serviceName=GunService&sni=$sni#$user"
+systemctl restart vmess-grpc.service
+systemctl restart vless-grpc.service
+service cron restart
+clear
+echo -e "================================="
+echo -e "            XRAY GRPC            " 
+echo -e "================================="
+echo -e "Remarks           : ${user}"
+echo -e "Domain            : ${domain}"
+echo -e "Port VMess        : ${tls}"
+echo -e "Port VLess        : $vl"
+echo -e "ID                : ${uuid}"
+echo -e "Alter ID          : 0"
+echo -e "Mode              : Gun"
+echo -e "Security          : TLS"
+echo -e "Type              : grpc"
+echo -e "Service Name      : GunService"
+echo -e "SNI               : $sni"
+echo -e "================================="
+echo -e "Link VMess GRPC  : "
+echo -e "${vmesslink1}"
+echo -e "================================="
+echo -e "Link VLess GRPC  : "
+echo -e "${vlesslink1}"
+echo -e "================================="
+echo -e "Expired On     : $exp"
+echo -e "=================================" 
+echo -e "Script Mod By SL"
