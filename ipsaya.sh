1b7e0ad827798b8127e8f469560e89cddedbb23b
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-02-16 17:23:01 +0700

    Update ipsaya.sh

--- ipsaya.sh
+++ ipsaya.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
-ipv6addr=$(ip addr show dev venet0 | sed -e's/^.*inet6 \([^ ]*\)\/.*$/\1/;t;d')
-ipv4intr=$(ip a s eth0 | egrep -o 'inet [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d' ' -f2)
+ipv6aku=$(ip addr show eth0 | grep "inet6\b" | awk '{print $2}' | cut -d/ -f1)
+ipv4aku=(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
 ipaku=$(wget -qO- ipecho.net/plain)
+hostall=hostname --all-ip-addresses | awk '{print $0}'
+host1=hostname --all-ip-addresses | awk '{print $1}'
+host2=hostname --all-ip-addresses | awk '{print $2}'
+host3=hostname --all-ip-addresses | awk '{print $2}'
+host4=hostname --all-ip-addresses | awk '{print $2}'
+ipcidr=ip -4 -o addr show eth0 | awk '{print $4}'
 ports=${ipv4intr#*.*.*.}
 
-echo Alamat IPv6 eksternal anda adalah:
-echo $ipv6addr
+echo Alamat IPv6 internal anda adalah:
+echo $ipv6aku
 echo
-echo Alamat IPv4 internal anda adalah:
-echo $ipv4intr
+echo Alamat IPv4 internal/ Private anda adalah:
+echo $ipv4aku
 echo
-echo Alamat IPv4 eksternal anda adalah:
+echo Alamat IPv4 eksternal/ Publik anda adalah:
 echo $ipaku
+echo Semua Host adalah:
+echo $host0
+echo Host 1 adalah:
+echo $host1
+echo Host 2 adalah:
+echo $host2
+echo Host 3 adalah:
+echo $host3
+echo Host 4 adalah:
+echo $host4
+echo IP CIDR adalah:
+echo $ipcidr
 echo
 echo NAT VPS anda bisa diakses dari port berikut:
 echo [$ipaku]:$ports\01 - $ports\20
