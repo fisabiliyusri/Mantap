ada72115b653c8774a5d2571e5b15076544914f5
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-02-16 17:46:37 +0700

    Update ipsaya.sh

--- ipsaya.sh
+++ ipsaya.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
+
+echo Semua Port TCP Yang Aktif:
+echo $slport
+echo 
+
 ipv6aku=$(ip addr show eth0 | grep "inet6\b" | awk '{print $2}' | cut -d/ -f1)
-ipv4aku=(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+ipv4aku=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
 ipaku=$(wget -qO- ipecho.net/plain)
-hostall=hostname --all-ip-addresses | awk '{print $0}'
-host1=hostname --all-ip-addresses | awk '{print $1}'
-host2=hostname --all-ip-addresses | awk '{print $2}'
-host3=hostname --all-ip-addresses | awk '{print $2}'
-host4=hostname --all-ip-addresses | awk '{print $2}'
-ipcidr=ip -4 -o addr show eth0 | awk '{print $4}'
+hostall=$(hostname --all-ip-addresses | awk '{print $0}')
+host1=$(hostname --all-ip-addresses | awk '{print $1}')
+host2=$(hostname --all-ip-addresses | awk '{print $2}')
+host3=$(hostname --all-ip-addresses | awk '{print $2}')
+host4=$(hostname --all-ip-addresses | awk '{print $2}')
+ipcidr=$(ip -4 -o addr show eth0 | awk '{print $4}')
 slport=sudo lsof -nP -iTCP -sTCP:LISTEN
 
+echo 
 echo Alamat IPv6 internal anda adalah:
 echo $ipv6aku
 echo
@@ -18,6 +24,7 @@ echo $ipv4aku
 echo
 echo Alamat IPv4 eksternal/ Publik anda adalah:
 echo $ipaku
+echo
 echo Semua Host adalah:
 echo $host0
 echo Host 1 adalah:
@@ -31,5 +38,3 @@ echo $host4
 echo IP CIDR adalah:
 echo $ipcidr
 echo
-echo Semua Port TCP Yang Aktif:
-echo $slport
