c2583fc2b97bd0795edcedf3d5d0c54e5c873797
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-02-16 23:17:11 +0700

    Update grpcupdate.sh

--- update/grpcupdate.sh
+++ update/grpcupdate.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-# Getting
-# Update Script GRPC
-
-# Link Hosting Kalian Untuk Xray
-akbarvpnn="raw.githubusercontent.com/fisabiliyusri/Mantap/main/xray"
+# ==========================================
+# ==========================================
+# 
+cd
 
 cd /usr/bin
-wget -O addgrpc "https://${akbarvpnn}/addgrpc.sh"
-wget -O cekgrpc "https://${akbarvpnn}/cekgrpc.sh"
-wget -O delgrpc "https://${akbarvpnn}/delgrpc.sh"
-wget -O renewgrpc "https://${akbarvpnn}/renewgrpc.sh"
+
+wget -O addgrpc "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/xray/addgrpc.sh"
+wget -O cekgrpc "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/xray/cekgrpc.sh"
+wget -O delgrpc "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/xray/delgrpc.sh"
+wget -O renewgrpc "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/xray/renewgrpc.sh"
+
 
 chmod +x addgrpc
 chmod +x delgrpc
 chmod +x cekgrpc
 chmod +x renewgrpc
+
 cd
