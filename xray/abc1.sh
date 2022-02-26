5fa8eee2288d65d8862a341fd7f405c121e0d28d
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-02-26 18:00:29 +0700

    Update abc1.sh

--- xray/abc1.sh
+++ xray/abc1.sh
@@ -1,41 +1,18 @@
 #!/bin/bash
-RED='\e[1;31m'
-GREEN='\e[0;32m'
-BLUE='\e[0;34m'
-NC='\e[0m'
+
 MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Checking VPS"
-CEKEXPIRED () {
-    today=$(date -d +1day +%Y-%m-%d)
-    Exp1=$(curl -sS https://raw.githubusercontent.com/Iansoftware/userip/main/bossip | grep $MYIP | awk '{print $3}')
-    if [[ $today < $Exp1 ]]; then
-    echo -e "\e[32mSTATUS SCRIPT AKTIF...\e[0m"
-    else
-    echo -e "\e[31mSCRIPT ANDA EXPIRED!\e[0m";
-    echo -e "\e[31mRenew IP letak tempoh banyak kit okay? hehe syg ktk #\e[0m"
-    exit 0
-fi
-}
-IZIN=$(curl -sS https://raw.githubusercontent.com/Iansoftware/userip/main/bossip | awk '{print $4}' | grep $MYIP)
-if [ $MYIP = $IZIN ]; then
-echo -e "\e[32mPermission Accepted...\e[0m"
-CEKEXPIRED
-else
-echo -e "\e[31mPermission Denied!\e[0m";
-echo -e "\e[31mDaftar IP dalam github lok sayang okay? mun dah daftar tapi masih juak permission denied refresh dolok website ya hehe. Love you #\e[0m"
-exit 0
-fi
 clear
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[0;100;33m     • CHANGE DOMAIN VPS •         \E[0m"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e ""  
-echo "Please Input Your Pointing Domain In Cloudflare "
-read -rp "Domain/Host: " -e host
-rm /etc/rare/xray/domain
-echo "$host" >> /etc/rare/xray/domain
-echo "IP=$host" >> /var/lib/premium-script/ipvps.conf
-domain=$(cat /etc/rare/xray/domain)
+domain=$(cat /etc/xray/domain)
+sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill
+cd /root/
+wget https://raw.githubusercontent.com/acmesh-official/acme.sh/master/acme.sh
+bash acme.sh --install
+rm acme.sh
+cd .acme.sh
+bash acme.sh --register-account -m senowahyu62@gmail.com
+bash acme.sh --issue --standalone -d $domain --force
+bash acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key
+
 #Update Sertificate SSL
 echo Starting Update SSL Sertificate
 sleep 3
@@ -48,9 +25,9 @@ systemctl stop v2ray.service
 systemctl stop trojan
 systemctl stop trojan.service
 sleep 2
-/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256 --server letsencrypt >> /etc/rare/tls/$domain.log
-~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/rare/xray/xray.crt --keypath /etc/rare/xray/xray.key --ecc
-cat /etc/rare/tls/$domain.log
+/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256 --server letsencrypt >> /etc/tls/$domain.log
+~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc
+cat /etc/tls/$domain.log
 systemctl daemon-reload
 systemctl restart trojan
 systemctl restart trojan.service
@@ -80,10 +57,10 @@ server {
 	root /usr/share/nginx/html;
 	location /s/ {
     		add_header Content-Type text/plain;
-    		alias /etc/rare/config-url/;
+    		alias /etc/config-url/;
     }
 
-    location /xraygrpc {
+    location /xrayvmessgrpc {
 		client_max_body_size 0;
 #		keepalive_time 1071906480m;
 		keepalive_requests 4294967296;
@@ -92,10 +69,10 @@ server {
  		lingering_close always;
  		grpc_read_timeout 1071906480m;
  		grpc_send_timeout 1071906480m;
-		grpc_pass grpc://127.0.0.1:31301;
+		grpc_pass grpc://127.0.0.1:7777;
 	}
 
-	location /xraytrojangrpc {
+	location /xrayvlessgrpc {
 		client_max_body_size 0;
 		# keepalive_time 1071906480m;
 		keepalive_requests 4294967296;
@@ -104,7 +81,7 @@ server {
  		lingering_close always;
  		grpc_read_timeout 1071906480m;
  		grpc_send_timeout 1071906480m;
-		grpc_pass grpc://127.0.0.1:31304;
+		grpc_pass grpc://127.0.0.1:8888;
 	}
 }
 server {
@@ -113,7 +90,7 @@ server {
 	root /usr/share/nginx/html;
 	location /s/ {
 		add_header Content-Type text/plain;
-		alias /etc/rare/config-url/;
+		alias /etc/config-url/;
 	}
 	location / {
 		add_header Strict-Transport-Security "max-age=15552000; preload" always;
@@ -185,9 +162,5 @@ systemctl daemon-reload
 service nginx restart
 echo -e "\033[32m[Info]\033[0m nginx Start Successfully !"
 echo ""
-echo "Location Your Domain : /var/lib/premium-script/ipvps.conf"
-echo ""
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo ""
-read -n 1 -s -r -p "Press any key to back on menu"
-m-domain
