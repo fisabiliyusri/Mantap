78cd93a1f3ae5179e6b69a85e3785d6de89773b8
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-02-27 23:13:55 +0700

    Update addgrpc.sh

--- xray/addgrpc.sh
+++ xray/addgrpc.sh
@@ -1,38 +1,21 @@
 #!/bin/bash
-# SL
-# ==========================================
-# Color
-RED='\033[0;31m'
-NC='\033[0m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHT='\033[0;37m'
-# ==========================================
-# Getting
-MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Checking VPS"
-IZIN=$( curl ipinfo.io/ip | grep $MYIP )
-if [ $MYIP = $MYIP ]; then
-echo -e "${NC}${GREEN}Permission Accepted...${NC}"
-else
-echo -e "${NC}${RED}Permission Denied!${NC}";
-echo -e "${NC}${LIGHT}Fuck You!!"
-exit 0
-fi
+#sl
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+#########################
+
+
+MYIP=$(curl -sS ipv4.icanhazip.com)
 clear
-source /var/lib/crot/ipvps.conf
-if [[ "$IP" = "" ]]; then
 domain=$(cat /etc/xray/domain)
-else
-domain=$IP
-fi
-tls=$(cat /etc/xray/xray-grpc.json | grep port | awk '{print $2}' | sed 's/,//g')
+tls=$(cat /etc/xray/sl-vmessgrpc.json | grep port | awk '{print $2}' | sed 's/,//g')
+vl=$(cat /etc/xray/sl-vlessgrpc.json | grep port | awk '{print $2}' | sed 's/,//g')
 until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
 		read -rp "User: " -e user
-		CLIENT_EXISTS=$(grep -w $user /etc/xray/xray-grpc.json | wc -l)
+		CLIENT_EXISTS=$(grep -w $user /etc/xray/sl-vmessgrpc.json | wc -l)
 
 		if [[ ${CLIENT_EXISTS} == '1' ]]; then
 			echo ""
@@ -41,13 +24,11 @@ until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
 		fi
 	done
 uuid=$(cat /proc/sys/kernel/random/uuid)
-read -p "Expired (Days) : " masaaktif
-hariini=`date -d "0 days" +"%Y-%m-%d"`
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
 sed -i '/#vmessgrpc$/a\### '"$user $exp"'\
-},{"id": "'""$uuid""'","alterId": '"0"',"email": "'""$user""'"' /etc/xray/vmess-grpc.json
+},{"id": "'""$uuid""'","alterId": '"0"',"email": "'""$user""'"' /etc/xray/sl-vmessgrpc.json
 sed -i '/#vlessgrpc$/a\### '"$user $exp"'\
-},{"id": "'""$uuid""'","email": "'""$user""'"' /etc/xray/xray-grpc.json
+},{"id": "'""$uuid""'","email": "'""$user""'"' /etc/xray/sl-vlessgrpc.json
 cat > /etc/xray/$user-tls.json << EOF
       {
       "v": "0",
@@ -65,33 +46,32 @@ cat > /etc/xray/$user-tls.json << EOF
 EOF
 vmess_base641=$( base64 -w 0 <<< $vmess_json1)
 vmesslink1="vmess://$(base64 -w 0 /etc/xray/$user-tls.json)"
-vlesslink1="vless://${uuid}@${domain}:${tls}?mode=gun&security=tls&encryption=none&type=grpc&serviceName=GunService&sni=$sni#$user"
-systemctl restart vmess-grpc.service
-systemctl restart xray@grpc.service
+vlesslink1="vless://${uuid}@${domain}:${vl}?mode=gun&security=tls&encryption=none&type=grpc&serviceName=GunService&sni=${domain}#$user"
+systemctl restart sl-vmess-grpc.service
+systemctl restart sl-vless-grpc.service
 service cron restart
 clear
 echo -e "================================="
-echo -e "    XRAY/VMESS-GRPC/VLESS-GRPC   " 
+echo -e "            XRAY GRPC            " 
 echo -e "================================="
 echo -e "Remarks           : ${user}"
 echo -e "Domain            : ${domain}"
-echo -e "Port VMess GRPC   : ${tls}"
-echo -e "Port VLess GRPC   : $vl"
+echo -e "Port VMess        : ${tls}"
+echo -e "Port VLess        : ${vl}"
 echo -e "ID                : ${uuid}"
 echo -e "Alter ID          : 0"
 echo -e "Mode              : Gun"
 echo -e "Security          : TLS"
 echo -e "Type              : grpc"
 echo -e "Service Name      : GunService"
-echo -e "SNI               : $sni"
+echo -e "SNI               : ${domain}"
 echo -e "================================="
-echo -e "Link VMess GRPC   : "
+echo -e "Link VMess GRPC  : "
 echo -e "${vmesslink1}"
 echo -e "================================="
-echo -e "Link VLess GRPC   : "
+echo -e "Link VLess GRPC  : "
 echo -e "${vlesslink1}"
 echo -e "================================="
-echo -e "Created           : $hariini"
-echo -e "Expired On        : $exp"
+echo -e "Expired On     : $exp"
 echo -e "=================================" 
-echo -e "Script Mod By SL"
+echo -e " Script Mod By SL"
