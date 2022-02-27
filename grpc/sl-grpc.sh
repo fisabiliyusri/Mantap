2773c6fbba6252a7921bbc21f0244651c24af1c6
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-02-27 22:25:03 +0700

    Update sl-grpc.sh

--- grpc/sl-grpc.sh
+++ grpc/sl-grpc.sh
@@ -2,15 +2,32 @@
 # XRay Installation
 # SL Script
 # ==================================
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
 MYIP=$(wget -qO- ipinfo.io/ip);
 clear
 domain=$(cat /etc/xray/domain)
+apt install iptables iptables-persistent -y
+apt install curl socat xz-utils wget apt-transport-https gnupg gnupg2 gnupg1 dnsutils lsb-release -y 
+apt install socat cron bash-completion ntpdate -y
+ntpdate pool.ntp.org
+apt -y install chrony
+timedatectl set-ntp true
+systemctl enable chronyd && systemctl restart chronyd
+systemctl enable chrony && systemctl restart chrony
+timedatectl set-timezone Asia/Jakarta
+chronyc sourcestats -v
+chronyc tracking -v
+date
+
+# / / Ambil Xray Core Version Terbaru
+latest_version="$(curl -s https://api.github.com/repos/XTLS/Xray-core/releases | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
+
+# / / Installation Xray Core
+xraycore_link="https://github.com/XTLS/Xray-core/releases/download/v$latest_version/xray-linux-64.zip"
+
+# 
 uuid=$(cat /proc/sys/kernel/random/uuid)
 
-
+# / / Make Main Directory
 mkdir -p /usr/bin/xray
 mkdir -p /etc/xray
 
