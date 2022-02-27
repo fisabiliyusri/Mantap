fa524e2cb90b29e0f1f403158064cafc3dbb51cb
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-02-27 22:38:18 +0700

    Create sl-addgrpc.sh

---
+++ grpc/sl-addgrpc.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+#sl
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+#########################
+
+
+MYIP=$(curl -sS ipv4.icanhazip.com)
+clear
+domain=$(cat /etc/xray/domain)
+tls=$(cat /etc/xray/sl-vmessgrpc.json | grep port | awk '{print $2}' | sed 's/,//g')
+vl=$(cat /etc/xray/sl-vlessgrpc.json | grep port | awk '{print $2}' | sed 's/,//g')
+until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
+		read -rp "User: " -e user
+		CLIENT_EXISTS=$(grep -w $user /etc/xray/sl-vmessgrpc.json | wc -l)
+
+		if [[ ${CLIENT_EXISTS} == '1' ]]; then
+			echo ""
+			echo "A client with the specified name was already created, please choose another name."
+			exit 1
+		fi
+	done
+uuid=$(cat /proc/sys/kernel/random/uuid)
+exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
+sed -i '/#vmessgrpc$/a\### '"$user $exp"'\
+},{"id": "'""$uuid""'","alterId": '"0"',"email": "'""$user""'"' /etc/xray/sl-vmessgrpc.json
+sed -i '/#vlessgrpc$/a\### '"$user $exp"'\
+},{"id": "'""$uuid""'","email": "'""$user""'"' /etc/xray/sl-vlessgrpc.json
+cat > /etc/xray/$user-tls.json << EOF
+      {
+      "v": "0",
+      "ps": "${user}",
+      "add": "${domain}",
+      "port": "${tls}",
+      "id": "${uuid}",
+      "aid": "0",
+      "net": "grpc",
+      "path": "GunService",
+      "type": "none",
+      "host": "${sni}",
+      "tls": "tls"
+}
+EOF
+vmess_base641=$( base64 -w 0 <<< $vmess_json1)
+vmesslink1="vmess://$(base64 -w 0 /etc/xray/$user-tls.json)"
+vlesslink1="vless://${uuid}@${domain}:${vl}?mode=gun&security=tls&encryption=none&type=grpc&serviceName=GunService&sni=${domain}#$user"
+systemctl restart sl-vmess-grpc.service
+systemctl restart sl-vless-grpc.service
+service cron restart
+clear
+echo -e "================================="
+echo -e "            XRAY GRPC            " 
+echo -e "================================="
+echo -e "Remarks           : ${user}"
+echo -e "Domain            : ${domain}"
+echo -e "Port VMess        : ${tls}"
+echo -e "Port VLess        : ${vl}"
+echo -e "ID                : ${uuid}"
+echo -e "Alter ID          : 0"
+echo -e "Mode              : Gun"
+echo -e "Security          : TLS"
+echo -e "Type              : grpc"
+echo -e "Service Name      : GunService"
+echo -e "SNI               : ${domain}"
+echo -e "================================="
+echo -e "Link VMess GRPC  : "
+echo -e "${vmesslink1}"
+echo -e "================================="
+echo -e "Link VLess GRPC  : "
+echo -e "${vlesslink1}"
+echo -e "================================="
+echo -e "Expired On     : $exp"
+echo -e "=================================" 
+echo -e " Script Mod By SL"
