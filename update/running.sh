ea6664bcf8c8ee7117778b5d030b17f104aab3a7
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-02-27 12:17:39 +0700

    Update running.sh

--- update/running.sh
+++ update/running.sh
@@ -28,15 +28,6 @@ green='\e[1;32m'
 NC='\e[0m'
 green() { echo -e "\\033[32;1m${*}\\033[0m"; }
 red() { echo -e "\\033[31;1m${*}\\033[0m"; }
-PERMISSION
-
-if [ -f /home/needupdate ]; then
-red "Your script need to update first !"
-exit 0
-elif [ "$res" = "Permission Accepted..." ]; then
-echo -ne
-else
-red "Permission Denied!"
 exit 0
 fi
 clear
