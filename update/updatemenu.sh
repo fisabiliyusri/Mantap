5589d791fe5d326d0f9f099a1c2a298ee31befe1
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-02-27 16:27:28 +0700

    Update updatemenu.sh

--- update/updatemenu.sh
+++ update/updatemenu.sh
@@ -3,7 +3,7 @@
 # Color
 
 cd /usr/bin
-
+wget -O menu "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/update/menu.sh"
 wget -O ipsaya "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/update/ipsaya.sh"
 wget -O sshovpnmenu "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/update/sshovpn.sh"
 wget -O l2tpmenu "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/update/l2tpmenu.sh"
@@ -23,6 +23,7 @@ wget -O slowdnsmenu "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main
 wget -O running "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/update/running.sh"
 wget -O updatemenu "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/update/updatemenu.sh"
 
+chmod +x menu
 chmod +x ipsaya
 chmod +x sshovpnmenu
 chmod +x l2tpmenu
