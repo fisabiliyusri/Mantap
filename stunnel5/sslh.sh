e83c3a4ff1e3490e9c2bc0d1196ab909753657b2
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-02-28 16:44:30 +0700

    Update sslh.sh

--- stunnel5/sslh.sh
+++ stunnel5/sslh.sh
@@ -16,6 +16,7 @@ RUN=yes
 # binary to use: forked (sslh) or single-thread (sslh-select) version
 # systemd users: don't forget to modify /lib/systemd/system/sslh.service
 DAEMON=/usr/sbin/sslh
+
 DAEMON_OPTS="--user sslh --listen 0.0.0.0:443 --ssl 127.0.0.1:777 --ssh 127.0.0.1:109 --openvpn 127.0.0.1:1194 --http 127.0.0.1:8880 --pidfile /var/run/sslh/sslh.pid -n"
 END
 
