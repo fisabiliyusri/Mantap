b209a7dd21867377c80f799f581dfa19ad3b9e5f
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-03-03 15:21:32 +0700

    Update sslh-fix-reboot.sh

--- sslh-fix/sslh-fix-reboot.sh
+++ sslh-fix/sslh-fix-reboot.sh
@@ -2,13 +2,18 @@
 # ==========================================
 
 systemctl stop ws-tls 
-&& 
+pkill python
+systemctl stop sslh
+systemctl daemon-reload
+systemctl disable ws-tls
+systemctl disable sslh
+systemctl daemon-reload
+systemctl enable sslh
+systemctl enable ws-tls
 systemctl start sslh 
-&& 
 /etc/init.d/sslh start 
-&& 
 /etc/init.d/sslh restart 
-&& 
 systemctl daemon-reload 
-&& 
 systemctl start ws-tls
+systemctl restart ws-tls
+restart
