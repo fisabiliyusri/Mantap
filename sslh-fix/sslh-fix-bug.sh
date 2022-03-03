1cdde6bebdc0dce79b084eba47a9d0aa163c4cd0
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-03-03 14:26:40 +0700

    Update sslh-fix-bug.sh

--- sslh-fix/sslh-fix-bug.sh
+++ sslh-fix/sslh-fix-bug.sh
@@ -2,4 +2,4 @@
 # ==========================================
 
 
-echo "0 5 * * * root clearlog && cd && ./sslh-fix-reboot.sh" >> /etc/crontab
+echo "0 4 * * * root clearlog && cd && ./sslh-fix-reboot.sh" >> /etc/crontab
