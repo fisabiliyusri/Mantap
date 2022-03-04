61d9cff64a4b4995adeb5ac2e3bfe9a854b7ac7f
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-03-04 19:59:48 +0700

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -170,6 +170,8 @@ screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 500
 # setting port ssh
 sed -i 's/Port 22/Port 22/g' /etc/ssh/sshd_config
 sed -i '/Port 22/a Port 2253' /etc/ssh/sshd_config
+echo "Port 22" >>/etc/ssh/sshd_config
+echo "Port 42" >>/etc/ssh/sshd_config
 /etc/init.d/ssh restart
 
 # install dropbear
