ec8a49014b11db96356a8206f92008c83f0ddb47
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-03-04 15:59:21 +0700

    Update menu.sh

--- update/menu.sh
+++ update/menu.sh
@@ -6,6 +6,7 @@ yy="\033[0;1;32m"
 yl="\033[0;1;33m"
 wh="\033[0m"
 echo -e "$y                  MENU UTAMA $wh"
+echo -e "$y            Script Mod By SL $wh"
 echo -e "$y-------------------------------------------------$wh"
 echo -e "$yy 1$y.  SSH & OpenVPN MENU  $wh"
 echo -e "$yy 2$y.  L2TP MENU$wh"
@@ -14,20 +15,21 @@ echo -e "$yy 4$y.  SSTP MENU$wh"
 echo -e "$yy 5$y.  WIREGUARD MENU$wh"
 echo -e "$yy 6$y.  SHADOWSOCKS MENU$wh"
 echo -e "$yy 7$y.  SHADOWSOCKSR MENU$wh"
-echo -e "$yy 8$y.  VMESS MENU$wh"
-echo -e "$yy 9$y.  VLESS MENU$wh"
-echo -e "$yy 10$y. TROJAN GFW MENU$wh"
+echo -e "$yy 8$y.  XRAY VMESS MENU$wh"
+echo -e "$yy 9$y.  XRAY VLESS MENU$wh"
+echo -e "$yy 10$y. XRAY TROJAN MENU$wh"
 echo -e "$yy 11$y. TROJAN GO MENU$wh"
-echo -e "$yy 12$y. GRPC MENU$wh"
-echo -e "$yy 13$y. SLOWDNS MENU$wh"
+echo -e "$yy 12$y. XRAY GRPC MENU$wh"
+echo -e "$yy 13$y. SLOWDNS MENU (OFF)$wh"
 echo -e "$yy 14$y. CEK SEMUA IP PORT$wh"
 echo -e "$yy 15$y. CEK SEMUA SERVICE VPN$wh"
-echo -e "$yy 16$y. UPDATE MENU$wh"
-echo -e "$yy 17$y. SL Menu(Perbaiki Error SSLH+WS)$wh"
-echo -e "$yy 18$y. Settings$wh"
-echo -e "$yy 19$y. Exit$wh"
+echo -e "$yy 16$y. UPDATE MENU (2x)$wh"
+echo -e "$yy 17$y. SL-FIX (Perbaiki Error SSLH+WS-TLS setelah reboot)$wh"
+echo -e "$yy 18$y. Settings (Pengaturan)$wh"
+echo -e "$yy 19$y. Exit (Keluar)$wh"
+echo -e "$yy 20$y. Salin Repo Script Mantap$wh"
 echo -e "$y-------------------------------------------------$wh"
-read -p "Select From Options [ 1 - 19 ] : " menu
+read -p "Select From Options [ 1 - 20 ] : " menu
 case $menu in
 1)
 clear
@@ -104,6 +106,10 @@ setmenu
 19)
 clear
 exit
+;;
+20)
+clear
+copyrepo
 ;;
 *)
 clear
