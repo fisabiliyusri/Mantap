0e45ea45cba7f36a4472c4e65ead0a95768f0cec
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-03-05 17:24:52 +0700

    Update menuinfo.sh

--- update/menuinfo.sh
+++ update/menuinfo.sh
@@ -1,28 +1,30 @@
-
+#!/bin/bash
+domain=$(cat /etc/xray/domain)
 
 #Payload
+echo -e "Info Opok Tsel:"
+echo -e "Methode SSL/TLS Websocket Bug Cloudflare"
+echo -e "Payload SSL:"
+echo -e "GET wss://covid19.go.id/ HTTP/1.1[crlf]Host: $domain[crlf]Upgrade: websocket[crlf][crlf]"
+echo -e "SNI/Server Spoof:"
+echo -e "covid19.go.id"
+echo -e "Server Remote:"
+echo -e "covid19.go.id:443"
 
-Opok Tsel:
-Payload SSL:
-GET wss://covid19.go.id/ HTTP/1.1[crlf]Host: $domain[crlf]Upgrade: websocket[crlf][crlf]
-SNI/Server Spoof:
-covid19.go.id
-Server Remote:
-covid19.go.id:443
+echo -e "Exra Unli:"
+echo -e "Methode SSL/TLS Websocket Bug Cloudflare"
+echo -e "GET wss://v27.tiktokcdn.com/ HTTP/1.1[crlf]Host: $domain[crlf]Upgrade: websocket[crlf][crlf]"
+echo -e "SNI/Server Spoof:"
+echo -e "v27.tiktokcdn.com"
+echo -e "Server Remote:"
+echo -e "v27.tiktokcdn.com:443"
 
-Exra Unli:
-GET wss://v27.tiktokcdn.com/ HTTP/1.1[crlf]Host: $domain[crlf]Upgrade: websocket[crlf][crlf]
-SNI/Server Spoof:
-v27.tiktokcdn.com
-Server Remote:
-v27.tiktokcdn.com:443
-
-Contoh Payload SSH SSL:
-GET wss://ISI-DENGAN-BUG-WEBSOCKET-CLOUDFLARE/ HTTP/1.1[crlf]Host: ISI-DENGAN-NAMA-SUBDOMAIN-SSH-JANGANPAKEIP[crlf]Upgrade: websocket[crlf][crlf]
-SNI/Server Spoof:
-ISI-DENGAN-BUG-WEBSOCKET-CLOUDFLARE
-Server Remote:
-ISI-DENGAN-BUG-WEBSOCKET-CLOUDFLARE:443
+echo -e "Contoh Payload SSH SSL WEBSOCKET CLOUDFLARE:"
+echo -e "GET wss://ISI-DENGAN-BUG-WEBSOCKET-CLOUDFLARE/ HTTP/1.1[crlf]Host: ISI-DENGAN-NAMA-SUBDOMAIN-SSH-JANGANPAKEIP[crlf]Upgrade: websocket[crlf][crlf]"
+echo -e "SNI/Server Spoof:"
+echo -e "ISI-DENGAN-BUG-WEBSOCKET-CLOUDFLARE"
+echo -e "Server Remote:"
+echo -e "ISI-DENGAN-BUG-WEBSOCKET-CLOUDFLARE:443"
 
 STATUS RESPON WAKTU INJECK BUG,SSH,VPN
 301/302 = berarti BUG/PAYLOAD/Port nya di BLOKIR sementara oleh ISP
