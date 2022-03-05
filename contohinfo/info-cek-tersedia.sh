9646fcb99f832043861cdc28cad17191e098ff5a
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-03-05 18:54:16 +0700

    Update info-cek-tersedia.sh

--- contohinfo/info-cek-tersedia.sh
+++ contohinfo/info-cek-tersedia.sh
@@ -47,3 +47,4 @@ echo -e "521 - Origin Error = Berarti masalah nya ada di subdomain/domain"
 echo -e "mungkin gagal saat install certifikat nya cara atasi masalah nya buat subdomain lagi dengan cara ketik slhost lalu ketik certv2ray di vps,"
 echo -e "jika berhasil lalu reboot vps ,ketik reboot"
 echo -e ""
+menuinfo
