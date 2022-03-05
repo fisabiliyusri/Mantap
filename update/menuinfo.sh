e6813961d7b3d9b8e3da6a93c381fbc45625afc0
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-03-05 18:50:57 +0700

    Update menuinfo.sh

--- update/menuinfo.sh
+++ update/menuinfo.sh
@@ -16,9 +16,9 @@ echo -e "$yy 6$y. Info Status Respon Injeck $wh"
 echo -e "$yy 7$y. Menu$wh"
 echo -e "$yy 8$y. Exit$wh"
 echo -e "$y-------------------------------------------------------------$wh"
-read -p "Select From Options [ 1 - 8 ] : " menu
+read -p "Select From Options [ 1 - 8 ] : " menuinfo
 echo -e ""
-case $menu in
+case $menuinfo in
 1)
 info-tsel-opok
 ;;
