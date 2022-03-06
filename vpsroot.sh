bf56951eb86d55cd1091f05202244d036a085c58
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-03-06 16:02:08 +0700

    Update vpsroot.sh

--- vpsroot.sh
+++ vpsroot.sh
@@ -6,11 +6,19 @@ clear;
 echo -e "Masukkan Password:";
 read -e pwe;
 usermod -p `perl -e "print crypt("$pwe","Q4")"` root;
+echo -e "Masukan Nama User Akun Baru";
+read -e crot;
+sudo useradd -s /bin/bash -d /home/$crot/ -m -G sudo $crot;
 clear;
 printf "Mohon Simpan Informasi Akun VPS Ini
 ============================================
+Akun Root (Akun Utama)
 Ip address = $(curl -Ls http://ipinfo.io/ip)
 Username   = root
+Password   = $pwe
+============================================
+User Akun Baru Dengan Akses Root (Akun Baru)
+Username   = $crot
 Password   = $pwe
 ============================================";
 echo "";
