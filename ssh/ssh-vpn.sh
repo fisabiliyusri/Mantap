96983656ffc975b5f44fae273691965782c96bc1
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-03-07 21:44:12 +0700

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -519,6 +519,12 @@ chmod +x renewtrgo
 chmod +x cektrgo
 echo "0 5 * * * root clearlog && reboot" >> /etc/crontab
 echo "0 0 * * * root xp" >> /etc/crontab
+echo "10 4 * * * root clearlog && sslh-fix-reboot" >> /etc/crontab
+echo "0 0 * * * root clearlog && reboot" >> /etc/crontab
+echo "0 12 * * * root clearlog && reboot" >> /etc/crontab
+echo "0 18 * * * root clearlog && reboot" >> /etc/crontab
+
+
 # remove unnecessary files
 cd
 apt autoclean -y
