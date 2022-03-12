934e1cb4a590c7ca4009d1dc18fc913b4e8b93e1
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-03-13 02:00:02 +0700

    Update fb-delgrpc.sh

--- grpc/fb-delgrpc.sh
+++ grpc/fb-delgrpc.sh
@@ -31,15 +31,14 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/fb-vmessgrpc.json")
 		echo "You have no existing clients!"
 		exit 1
 	fi
-	done
 	
+	clear
 NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/fb-vlessgrpc.json")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		echo ""
 		echo "You have no existing clients!"
 		exit 1
 	fi
-	done
 	
 	clear
 	echo ""
