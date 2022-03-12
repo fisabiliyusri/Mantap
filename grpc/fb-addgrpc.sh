649624da9f19f363bed96778eaa2f2565626780c
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-03-13 00:42:49 +0700

    Update fb-addgrpc.sh

--- grpc/fb-addgrpc.sh
+++ grpc/fb-addgrpc.sh
@@ -46,6 +46,7 @@ cat > /etc/xray/$user-tls.json << EOF
 EOF
 vmess_base641=$( base64 -w 0 <<< $vmess_json1)
 vmesslink1="vmess://$(base64 -w 0 /etc/xray/$user-tls.json)"
+vmesslink1="vmess://${uuid}@${domain}:${tls}/?type=grpc&encryption=auto&serviceName=GunService&security=tls&sni=${domain}#$user"
 vlesslink1="vless://${uuid}@${domain}:${vl}?mode=gun&security=tls&encryption=none&type=grpc&serviceName=GunService&sni=${domain}#$user"
 systemctl restart fb-vmess-grpc.service
 systemctl restart fb-vless-grpc.service
