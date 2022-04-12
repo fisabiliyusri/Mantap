d63a9d570c778d601abf05bbcf42b6dd213a8805
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-04-13 01:58:18 +0700

    Update cf.sh

--- ssh/cf.sh
+++ ssh/cf.sh
@@ -24,11 +24,15 @@ exit 0
 fi
 clear
 apt install jq curl -y
-DOMAIN=installer.my.id
+###DOM#AIN=installer.my.id
+DOMAIN=slxray.cf
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c6)
-SUB_DOMAIN=${sub}.installer.my.id
-CF_ID=senowahyu62@gmail.com
-CF_KEY=84570683099becbeb0f04d8086ce6f309c56c
+SUB_DOMAIN=${sub}.slxray.cf
+CF_ID=slinfinity69@gmail.com
+CF_KEY=3dcb550a77cb0a64dacf205243e3ac550f9db
+##SUB_DOMAIN=${sub}.installer.my.id
+##CF_ID=senowahyu62@gmail.com
+##CF_KEY=84570683099becbeb0f04d8086ce6f309c56c
 set -euo pipefail
 IP=$(wget -qO- ipinfo.io/ip);
 echo "Updating DNS for ${SUB_DOMAIN}..."
