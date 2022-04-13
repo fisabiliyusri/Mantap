b3e63e72fe2952b4fb69afe1173823f75ec8882e
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-04-13 07:26:28 +0700

    Update and rename fb-addgrpc.sh to addgrpc.sh

--- grpc/addgrpc.sh
+++ grpc/addgrpc.sh
@@ -11,11 +11,11 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 MYIP=$(curl -sS ipv4.icanhazip.com)
 clear
 domain=$(cat /etc/xray/domain)
-tls=$(cat /etc/xray/fb-vmessgrpc.json | grep port | awk '{print $2}' | sed 's/,//g')
-vl=$(cat /etc/xray/fb-vlessgrpc.json | grep port | awk '{print $2}' | sed 's/,//g')
+tls=$(cat /etc/xray/vmessgrpc.json | grep port | awk '{print $2}' | sed 's/,//g')
+vl=$(cat /etc/xray/vlessgrpc.json | grep port | awk '{print $2}' | sed 's/,//g')
 until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
 		read -rp "User: " -e user
-		CLIENT_EXISTS=$(grep -w $user /etc/xray/fb-vmessgrpc.json | wc -l)
+		CLIENT_EXISTS=$(grep -w $user /etc/xray/vmessgrpc.json | wc -l)
 
 		if [[ ${CLIENT_EXISTS} == '1' ]]; then
 			echo ""
@@ -26,9 +26,9 @@ until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
 uuid=$(cat /proc/sys/kernel/random/uuid)
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
 sed -i '/#vmessgrpc$/a\### '"$user $exp"'\
-},{"id": "'""$uuid""'","alterId": '"0"',"email": "'""$user""'"' /etc/xray/fb-vmessgrpc.json
+},{"id": "'""$uuid""'","alterId": '"0"',"email": "'""$user""'"' /etc/xray/vmessgrpc.json
 sed -i '/#vlessgrpc$/a\### '"$user $exp"'\
-},{"id": "'""$uuid""'","email": "'""$user""'"' /etc/xray/fb-vlessgrpc.json
+},{"id": "'""$uuid""'","email": "'""$user""'"' /etc/xray/vlessgrpc.json
 cat > /etc/xray/$user-tls.json << EOF
       {
       "v": "0",
