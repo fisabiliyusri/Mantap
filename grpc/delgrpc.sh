d70d643a54cb9b9fae830e040ace1a569261774c
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-04-13 07:30:29 +0700

    Update and rename fb-delgrpc.sh to delgrpc.sh

--- grpc/delgrpc.sh
+++ grpc/delgrpc.sh
@@ -25,7 +25,7 @@ echo -e "${NC}${LIGHT}Fuck you!!"
 exit 0
 fi
 clear
-NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/fb-vmessgrpc.json")
+NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/vmessgrpc.json")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		echo ""
 		echo "You have no existing clients!"
@@ -33,7 +33,7 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/fb-vmessgrpc.json")
 	fi
 	
 	clear
-NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/fb-vlessgrpc.json")
+NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/vlessgrpc.json")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		echo ""
 		echo "You have no existing clients!"
@@ -46,8 +46,8 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/fb-vlessgrpc.json")
 	echo " Press CTRL+C to return"
 	echo " ==============================="
 	echo "     No  Expired   User"
-	grep -E "^### " "/etc/xray/fb-vmessgrpc.json" | cut -d ' ' -f 2-3 | nl -s ') '
-	grep -E "^### " "/etc/xray/fb-vlessgrpc.json" | cut -d ' ' -f 2-3 | nl -s ') '
+	grep -E "^### " "/etc/xray/vmessgrpc.json" | cut -d ' ' -f 2-3 | nl -s ') '
+	grep -E "^### " "/etc/xray/vlessgrpc.json" | cut -d ' ' -f 2-3 | nl -s ') '
 	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
 		if [[ ${CLIENT_NUMBER} == '1' ]]; then
 			read -rp "Select one client [1]: " CLIENT_NUMBER
@@ -55,15 +55,15 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/fb-vlessgrpc.json")
 			read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
 		fi
 	done
-user=$(grep -E "^### " "/etc/xray/fb-vmessgrpc.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
-exp=$(grep -E "^### " "/etc/xray/fb-vmessgrpc.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
-user=$(grep -E "^### " "/etc/xray/fb-vlessgrpc.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
-exp=$(grep -E "^### " "/etc/xray/fb-vlessgrpc.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
-sed -i "/^### $user $exp/,/^},{/d" /etc/xray/fb-vmessgrpc.json
-sed -i "/^### $user $exp/,/^},{/d" /etc/xray/fb-vlessgrpc.json
+user=$(grep -E "^### " "/etc/xray/vmessgrpc.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
+exp=$(grep -E "^### " "/etc/xray/vmessgrpc.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+user=$(grep -E "^### " "/etc/xray/vlessgrpc.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
+exp=$(grep -E "^### " "/etc/xray/vlessgrpc.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+sed -i "/^### $user $exp/,/^},{/d" /etc/xray/vmessgrpc.json
+sed -i "/^### $user $exp/,/^},{/d" /etc/xray/vlessgrpc.json
 rm -f /etc/xray/$user-tls.json
-systemctl restart fb-vmess-grpc.service
-systemctl restart fb-vless-grpc.service
+systemctl restart vmess-grpc.service
+systemctl restart vless-grpc.service
 clear
 echo ""
 echo "==============================="
