945d84d10e09915b35438f8a15ce8b402a31dadd
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-04-17 23:17:15 +0700

    Update and rename shadowhost to shadowhost.sh

--- shadowsocks-plugin/shadowhost.sh
+++ shadowsocks-plugin/shadowhost.sh
@@ -1,3 +1,17 @@
+#!/bin/bash
+# Mod By SL
+# =====================================================
+
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+
 MYIP=$(wget -qO- icanhazip.com);
 apt install jq curl -y
 rm -rf /etc/shadowsocks/domain
