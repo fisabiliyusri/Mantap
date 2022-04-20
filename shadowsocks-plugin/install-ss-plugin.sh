bf0c9674ffb2abd22f86d0888cc40a8175cacac4
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-04-20 07:09:12 +0700

    Update install-ss-plugin.sh

--- shadowsocks-plugin/install-ss-plugin.sh
+++ shadowsocks-plugin/install-ss-plugin.sh
@@ -96,12 +96,12 @@ wget -O xray-plugin-universal.apk "https://raw.githubusercontent.com/fisabiliyus
 
 sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill
 # // Certificate File
-#cd /root/
-#wget -O acme.sh https://raw.githubusercontent.com/acmesh-official/acme.sh/master/acme.sh
-#bash acme.sh --install
-#rm acme.sh
-#cd .acme.sh
-#bash acme.sh --register-account -m slinfinity69@gmail.com
-#bash acme.sh --issue --standalone -d $domain --force
-#bash acme.sh --installcert -d $domain --fullchainpath /etc/shadowsocks/xray.crt --keypath /etc/shadowsocks/xray.key
+cd /root/
+wget -O acme.sh https://raw.githubusercontent.com/acmesh-official/acme.sh/master/acme.sh
+bash acme.sh --install
+rm acme.sh
+cd .acme.sh
+bash acme.sh --register-account -m slinfinity69@gmail.com
+bash acme.sh --issue --standalone -d $domain --force
+bash acme.sh --installcert -d $domain --fullchainpath /etc/shadowsocks/xray.crt --keypath /etc/shadowsocks/xray.key
 
