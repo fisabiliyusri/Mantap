a718967c39b187e0fcbbc03f79b72a6a798f2bb0
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-05-07 11:07:50 +0700

    Update menu.sh

--- update/menu.sh
+++ update/menu.sh
@@ -29,7 +29,7 @@ echo -e "$yy 18$y. Settings (Pengaturan)$wh"
 echo -e "$yy 19$y. Exit (Keluar)$wh"
 echo -e "$yy 20$y. copyrepo (Salin Repo Script Mantap)$wh"
 echo -e "$yy 21$y. menuinfo (Untuk Mendapatkan Informasi)$wh"
-echo -e "$yy 22$y. Shadowsocks Plugin Menu$wh"
+echo -e "$yy 22$y. Shadowsocks Plugin (Buat Akun)$wh"
 echo -e "$y-------------------------------------------------$wh"
 read -p "Select From Options [ 1 - 22 ] : " menu
 case $menu in
