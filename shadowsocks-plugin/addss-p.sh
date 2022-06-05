b1dd1e26ec51c1a70de278c094a6a43b13c2d8c1
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-06-06 00:45:57 +0700

    Update addss-p.sh

--- shadowsocks-plugin/addss-p.sh
+++ shadowsocks-plugin/addss-p.sh
@@ -157,7 +157,7 @@ gosth2="$((blastport3+1))"
 fi
 source /var/lib/crot/ipvps.conf
 if [[ "$IP2" = "" ]]; then
-domain=$(cat /etc/shadowsocks/domain)
+domain=$(cat /etc/xray/domain)
 else
 domain=$IP2
 fi
