aaff1a79f360d526804bfc248db00f5dde82a0c3
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-07-02 14:42:50 +0700

    Update vpsroot.sh

--- vpsroot.sh
+++ vpsroot.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 # Mod By SL
-# User Akun Baru Dengan Akses Root (Akun Baru);
-# Username   = $crot ;
-# Password   = $Pass ;
-# ============================================";
-#echo -e "Masukan Nama User Akun Baru";
-#read -p "Password : " Pass
-#read -e crot;
-#sudo useradd -s /bin/bash -d /home/$crot/ -m -G sudo $crot;
-#echo -e "$Pass\n$Pass\n"|passwd $crot &> /dev/null
-#usermod -p `perl -e "print crypt("$Pass","Q4")"` $crot;
-#usermod -aG sudo $crot;
 #echo "$crot    ALL=(ALL:ALL) ALL" >> /etc/sudoers;
 wget -qO- -O /etc/ssh/sshd_config https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/sshd_config;
 systemctl restart sshd;
